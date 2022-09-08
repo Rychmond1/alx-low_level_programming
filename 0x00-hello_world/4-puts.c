d12a44e524d5696f7472a820cafc18e8b8522c04
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-08 12:08:24 +0000

    Task 4 fix i hope

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,8 +2,8 @@
 /**
  *  * main - Entry point
  *   *
- *    * Return: always 0
- *     */
+ *    * Return: always 0* */
+ 
 int main(void)
 {
 		puts("\"Programming is like building a multilingual puzzle");
