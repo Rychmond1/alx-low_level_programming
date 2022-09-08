ca7a88698da2bb63cd79054015af2b74131af72c
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-08 12:27:02 +0000

    Task 6 Fix

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: always 0
- *     */
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 		printf("Size of a char: %d byte(s)\n", sizeof(char));
