2b306a8b5277455359a159a453a1d71062fed196
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-08 12:23:06 +0000

    Task 101 Fix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,10 +2,10 @@
 #include <unistd.h>
 
 /**
- *  * main - Entry point
- *   *
- *    * Return: Always 1 (Success)
- *     */
+ * main - Entry point
+ *
+ * Return: Always 1 (Success)
+ */
 int main(void)
 {
 		write(2,
