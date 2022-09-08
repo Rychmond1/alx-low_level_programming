afc9966a27220f2565b3306b3c44d62ebe80ee3d
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-08 12:20:32 +0000

    Task 5 Fix

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *  * main - Entry point
- *   *
- *    * Return: always 0
- *     */
+ * main - Entry point
+ *
+ * Return: always 0
+ */
 int main(void)
 {
 		printf("with proper grammar, but the outcome is a piece of art,\n");
