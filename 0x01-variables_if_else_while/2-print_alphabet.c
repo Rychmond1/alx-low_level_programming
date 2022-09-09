19efbff0a0e7afc27a67a8bc75b27722c5ce0de6
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-09 13:42:17 +0000

    j

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always return 0
+ *
+ * function main - list alphabets ato z
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a' ; a <= 'z' ; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
