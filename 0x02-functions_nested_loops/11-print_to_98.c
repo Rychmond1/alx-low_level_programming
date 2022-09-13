ca180840a4cb42795f2d8389a65312f22b6d0ae9
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-13 18:52:06 +0000

    leggo

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -16,12 +16,10 @@ void print_to_98(int n)
 		{
 			printf("%d, ", i);
 		}
-		else
-		{
-			for (i = n; i <= 98; i++)
-			{
-				printf("%d, ", i);
-			}
-		}
+
+	} else
+	{
+		for (i = n; i <= 98; i++)
+			printf("%d, ", i);
 	}
 }
