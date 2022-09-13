05789ce14fed59bcef367b78d09c7fe3d07f3359
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-13 15:07:42 +0000

    Task 4

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _islower - fuction to check if lowercase
+ * @c: is the aargument
+ *
+ * Return: 0
+ */
+int _islower(int c)
+{
+	if (i >= 'c'; &&c <= 'z')
+	{
+		return (1);
+	}
+	els
+		return (0);
+}
