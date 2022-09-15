00887e19aa1ebfaa0a125d4455f3f1096a19fc69
Richmond Peter <richmondpeter1000@gmail.com>
2022-09-15 12:55:39 +0000

    leggo

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isdidgit - checks whether digit or not
+ * @c: argument
+ * Return: 0 or 1
+ */
+int _isdigit(int c);
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	return (0);
+}
